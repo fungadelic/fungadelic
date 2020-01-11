8c01d2e108d770299fad47655c96013db9672225
Antonio <>
2020-01-11 19:55:13 +0100

    v8

---
+++ poblar_en.sql
@@ -0,0 +1,20 @@
+INSERT INTO version VALUES ("1.0");
+INSERT INTO language VALUES ("en");
+
+INSERT INTO colors VALUES (1,  'ffffff', 'White');
+INSERT INTO colors VALUES (2,  'cfcdc9', 'Light gray');
+INSERT INTO colors VALUES (3,  '58524b', 'Dark gray');
+INSERT INTO colors VALUES (4,  '934d11', 'Light brown');
+INSERT INTO colors VALUES (5,  '4e3524', 'Dark brown');
+INSERT INTO colors VALUES (6,  'ffb673', 'Light orange');
+INSERT INTO colors VALUES (7,  'cb6015', 'Dark orange');
+INSERT INTO colors VALUES (8,  'f9e27d', 'Light yellow');
+INSERT INTO colors VALUES (9,  'ffcd00', 'Dark yellow');
+INSERT INTO colors VALUES (10, 'e03c31', 'Red');
+
+INSERT INTO mushrooms VALUES ('Agaricus bisporus', 'Brown swiss', 1, 2, NULL, 1, 2, NULL, 1, 2, NULL, 0, NULL, '0001.jpg');
+INSERT INTO mushrooms VALUES ('Lactarius deliciosus', 'Saffron milk cap', 6, 7, NULL, 6, 7, NULL, 6, 7, NULL, 0, NULL, '0002.jpg');
+INSERT INTO mushrooms VALUES ('Omphalotus olearius', 'Jack o''lantern mushroom', 6, 7, NULL, 6, 7, NULL, 6, 7, NULL, 1, NULL, '0003.jpg');
+INSERT INTO mushrooms VALUES ('Amanita muscaria', NULL, 10, NULL, NULL, 1, 2, NULL, 1, 2, NULL, 1, 'Large mushroom, whose cap can reach 25 cm in diameter', '0004.jpg');
+INSERT INTO mushrooms VALUES ('Amanita pantherina', 'Panther cap', 4, NULL, NULL, 1, 2, NULL, 1, 2, NULL, 1, 'Large mushroom, whose cap can reach 10 cm in diameter', '0005.jpg');
+INSERT INTO mushrooms VALUES ('Amanita caesarea', 'Caesar''s mushroom', 6, 7, NULL, 6, 8, NULL, 6, 8, 1, 0, 'Large mushroom, whose cap can reach 25 cm in diameter. It may have white spots on the cap', '0006.jpg');
---
+++ poblar_es.sql
@@ -0,0 +1,20 @@
+INSERT INTO version VALUES ("1.0");
+INSERT INTO language VALUES ("es");
+
+INSERT INTO colors VALUES (1,  'ffffff', 'Blanco');
+INSERT INTO colors VALUES (2,  'cfcdc9', 'Gris claro');
+INSERT INTO colors VALUES (3,  '58524b', 'Gris oscuro');
+INSERT INTO colors VALUES (4,  '934d11', 'Marrón claro');
+INSERT INTO colors VALUES (5,  '4e3524', 'Marrón oscuro');
+INSERT INTO colors VALUES (6,  'ffb673', 'Naranja claro');
+INSERT INTO colors VALUES (7,  'cb6015', 'Naranja oscuro');
+INSERT INTO colors VALUES (8,  'f9e27d', 'Amarillo claro');
+INSERT INTO colors VALUES (9,  'ffcd00', 'Amarillo oscuro');
+INSERT INTO colors VALUES (10, 'e03c31', 'Rojo');
+
+INSERT INTO mushrooms VALUES ('Agaricus bisporus', 'Champiñón común', 1, 2, NULL, 1, 2, NULL, 1, 2, NULL, 0, NULL, '0001.jpg');
+INSERT INTO mushrooms VALUES ('Lactarius deliciosus', 'Níscalo', 6, 7, NULL, 6, 7, NULL, 6, 7, NULL, 0, NULL, '0002.jpg');
+INSERT INTO mushrooms VALUES ('Omphalotus olearius', 'Seta de olivo', 6, 7, NULL, 6, 7, NULL, 6, 7, NULL, 1, NULL, '0003.jpg');
+INSERT INTO mushrooms VALUES ('Amanita muscaria', NULL, 10, NULL, NULL, 1, 2, NULL, 1, 2, NULL, 1, 'Seta de gran tamaño, cuyo sombrero puede alcanzar los 25 cm de diámetro', '0004.jpg');
+INSERT INTO mushrooms VALUES ('Amanita pantherina', 'Amanita pantera', 4, NULL, NULL, 1, 2, NULL, 1, 2, NULL, 1, 'Seta de gran tamaño, cuyo sombrero puede alcanzar los 25 cm de diámetro tamaño, cuyo sombrero puede alcanzar los 10 cm de diámetro', '0005.jpg');
+INSERT INTO mushrooms VALUES ('Amanita caesarea', 'Amanita oronja', 6, 7, NULL, 6, 8, NULL, 6, 8, 1, 0, 'Seta de gran tamaño, cuyo sombrero puede alcanzar los 25 cm de diámetro. Puede presentar puntos o motas blancas sobre el sombrero', '0006.jpg');
